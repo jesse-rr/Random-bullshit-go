22473ab39419ea85a893e0e898abc00419d42b1c
hac1997 <herick.c1997@aluno.ifsc.edu.br>
2025-07-09 19:28:50 -0300

    branch herick apikey no gitignore

--- forecast-wtf/src/app/utils/api.ts
+++ forecast-wtf/src/app/utils/api.ts
@@ -1,6 +1,6 @@
 import type { CurrentWeather, DailyForecastDay, HourlyForecastHour, WeatherData } from "../types/weatherTypes";
 
-const API_KEY = '818a7ee5dbdf4e039f5225843250607'; 
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 const BASE_URL = 'https://api.weatherapi.com/v1';
 
 export const fetchWeatherData = async (city: string): Promise<WeatherData> => {
