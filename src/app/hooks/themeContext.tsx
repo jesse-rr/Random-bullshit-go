5d3588af06e51ed302642e88715fbe15cd2ea599
Herick <162757903+hac1997@users.noreply.github.com>
2025-07-08 21:28:00 -0300

    Updated themeContext.tsx

--- src/app/hooks/themeContext.tsx
+++ src/app/hooks/themeContext.tsx
@@ -1,12 +1,10 @@
-@@ .. @@
-   useEffect(() => {
+useEffect(() => {
      const savedTheme = localStorage.getItem('theme') as Theme | null;
--    if (savedTheme) {
-+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-+    
-+    if (savedTheme) {
+     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    
+     if (savedTheme) {
        setTheme(savedTheme);
-+    } else if (prefersDark) {
-+      setTheme('dark');
+     } else if (prefersDark) {
+       setTheme('dark');
      }
    }, []);
