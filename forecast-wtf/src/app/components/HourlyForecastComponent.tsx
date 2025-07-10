e6b07ea5dcf9f5d8f8d5998e25ebe3e0f3e8033c
nicolas <nicolasfvp@hotmail.com>
2025-07-09 21:36:01 -0300

    Ajustado .env e ajuste de erro

--- forecast-wtf/src/app/components/HourlyForecastComponent.tsx
+++ forecast-wtf/src/app/components/HourlyForecastComponent.tsx
@@ -2,7 +2,7 @@
 
 import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useRef, useState } from "react";
+import { HourlyForecastHour } from "../types/weatherTypes";
 
 interface HourlyForecastProps {
     forecast: HourlyForecastHour[];
@@ -14,12 +14,14 @@ export function HourlyForecastComponent({ forecast, loading, error }: HourlyFore
     if (loading) return <div className="flex items-center justify-center h-24 text-gray-600 dark:text-gray-300">Loading...</div>;
     if (error) return <div className="flex items-center justify-center h-24 text-red-500">Error: {error}</div>;
     if (!forecast || forecast.length === 0) return <div className="flex items-center justify-center h-24 text-gray-600 dark:text-gray-300">No data</div>;
-
+    console.log(forecast[0])
     return (
         <div className="w-full flex justify-center">
             <div className="flex overflow-x-auto gap-2 scrollbar-hide items-center px-3 py-2">
                 {forecast.map((hour) => (
+                    
                     <div 
+                        key={hour.time_epoch}
                         className="flex-shrink-0 flex flex-col items-center text-center w-[70px] h-[90px] p-2 rounded-lg shadow-sm"
                         >
                         <div className="text-xs font-medium text-gray-700 dark:text-gray-300 mb-1">{new Date(hour.time).getHours()}:00</div>
